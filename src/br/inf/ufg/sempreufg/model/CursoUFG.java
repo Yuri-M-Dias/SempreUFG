d26ca045df391b55f4919bfe62e87387c8a729f3
matcheus <mtesedd@hotmail.com>
2016-07-25 08:47:33 -0300

    Correção do commit

--- src/br/inf/ufg/sempreufg/model/CursoUFG.java
+++ src/br/inf/ufg/sempreufg/model/CursoUFG.java
@@ -1,7 +1,21 @@
 package br.inf.ufg.sempreufg.model;
 
-/**
- * Created by yuri on 18/07/16.
- */
+import java.sql.Date;
+
 public class CursoUFG {
+    
+    private String cufg_id, uafg_id, arco_id, cufg_nome, cufg_nivel, curs_turno;
+    private boolean cufg_presencial;
+    private Date cufg_data_criacao;
+
+    public CursoUFG(String cufg_id, String uafg_id, String arco_id, String cufg_nome, String cufg_nivel, String curs_turno, boolean cufg_presencial, Date cufg_data_criacao) {
+        this.cufg_id = cufg_id;
+        this.uafg_id = uafg_id;
+        this.arco_id = arco_id;
+        this.cufg_nome = cufg_nome;
+        this.cufg_nivel = cufg_nivel;
+        this.cufg_data_criacao = cufg_data_criacao;
+        this.cufg_presencial = cufg_presencial;
+        this.curs_turno = curs_turno;
+    }
 }
