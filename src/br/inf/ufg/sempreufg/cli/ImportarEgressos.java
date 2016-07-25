f2ef0f55221759cf9fec840f7b07e5bd07d831ae
Yuri-M-Dias <yurimathe.yp@gmail.com>
2016-07-25 09:30:12 -0300

    Adicionando lógica para o Reg.2

--- src/br/inf/ufg/sempreufg/cli/ImportarEgressos.java
+++ src/br/inf/ufg/sempreufg/cli/ImportarEgressos.java
@@ -1,11 +1,13 @@
 package br.inf.ufg.sempreufg.cli;
 
 import br.inf.ufg.sempreufg.auxiliar.ArquivoParaImportar;
-import br.inf.ufg.sempreufg.auxiliar.Parametros;
 import br.inf.ufg.sempreufg.conexao.Conexao;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -78,14 +80,9 @@ public class ImportarEgressos {
             preparedStatement.setString(1, nomeEgresso);
             preparedStatement.setString(2, tipoDocumento);
             preparedStatement.setString(3, numeroDocumento);
-            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-            Date date = format.parse(dataNascimento);
-            java.sql.Date dateSQL = new java.sql.Date(date.getTime());
+            java.sql.Date dateSQL = convertStringToSQLDate(dataNascimento);
             preparedStatement.setDate(4, dateSQL);
             preparedStatement.executeUpdate();
-        } catch (ParseException e) {
-            e.printStackTrace();
-            throw e;//rethrow
         }
         //Pega o id do curso da UFG...
         String nomeCursoUFG = listaCampos.get(4);
@@ -134,8 +131,66 @@ public class ImportarEgressos {
         }
     }
 
-    private static void inserirReg2(String registro) {
-        throw new NotImplementedException();
+    private static void inserirReg2(String registro) throws SQLException, ParseException {
+        List<String> listaCampos = Arrays.stream(registro.split("\\")).collect(toList());
+        listaCampos.remove(0);//Elimina "Reg.2"
+        String tipoDocumento = listaCampos.get(0);
+        String numeroDocumento = listaCampos.get(1);
+        String procuraEgressoSQL = "SELECT egre_id FROM public.egresso " +
+                "WHERE egre_tipo_doc_identidade = ? AND " +
+                "egre_numero_doc_identidade = ?";
+        String egressoId = null;
+        try (PreparedStatement preparedStatement = conexaoSQL.prepareStatement
+                (procuraEgressoSQL)) {
+            preparedStatement.setString(1, tipoDocumento);
+            preparedStatement.setString(2, numeroDocumento);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            egressoId = resultSet.getString(1);
+        }
+        if (egressoId == null) {
+            throw new SecurityException("Falha ao encontrar egresso.");
+        }
+        String cursoUFGId = listaCampos.get(2);
+        String procuraHistoricoUFGId = "SELECT hifg_id FROM public.historico_na_ufg " +
+                "WHERE egre_id = ? AND " +
+                "curs_id = ?";
+        String historicoId = null;
+        try (PreparedStatement preparedStatement = conexaoSQL.prepareStatement
+                (procuraHistoricoUFGId)) {
+            preparedStatement.setInt(1, Integer.parseInt(egressoId));
+            preparedStatement.setInt(2, Integer.parseInt(cursoUFGId));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            historicoId = resultSet.getString(1);
+        }
+        if (historicoId == null) {
+            throw new SecurityException("Esse egresso não cursou este curso " +
+                    "na UFG!");
+        }
+        String tipoRealizacao = listaCampos.get(3);
+        String dataInicio = listaCampos.get(4);
+        String dataFim = listaCampos.get(5);
+        String descricao = listaCampos.get(6);
+        String inserirRealizacaoSQL = "INSERT INTO public.realizacao_de_programa_academico(" +
+                "hifg_id, rpac_tipo, rpac_data_inicio, " +
+                "rpac_data_fim, rpac_descricao) " +
+                "VALUES (?, ?, ?, ?, ?)";
+        try (PreparedStatement preparedStatement = conexaoSQL.prepareStatement
+                (inserirRealizacaoSQL)) {
+            preparedStatement.setInt(1, Integer.valueOf(historicoId));
+            preparedStatement.setString(2, tipoRealizacao);
+            preparedStatement.setDate(3, convertStringToSQLDate(dataInicio));
+            preparedStatement.setDate(4, convertStringToSQLDate(dataFim));
+            preparedStatement.setString(5, descricao);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    private static java.sql.Date convertStringToSQLDate(String dataNascimento) throws ParseException {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = format.parse(dataNascimento);
+        java.sql.Date dateSQL = new java.sql.Date(date.getTime());
+        return dateSQL;
     }
 
 }
+
